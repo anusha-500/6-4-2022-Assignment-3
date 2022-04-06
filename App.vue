cc11de220285c71831b30ff9b4f5e9302d7d4bb3
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-06 18:22:57 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <que10 ></que10>
+    <que6 name="Anusha" mailid="Anusha@500apps" password="******"> </que6>
+    <que9></que9>
+    <que8></que8>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import que10 from './components/que10.vue'
+import que6 from './components/que6.vue'
+import que9 from './components/que9.vue'
+import que8 from './components/que8.vue'
+
+export default {
+  name: 'App',
+  components: {
+    que10,que6,que9,que8
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
