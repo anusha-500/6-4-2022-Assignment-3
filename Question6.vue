2e706e2dc13220e8d1c47f2fb0ca3b809b40f972
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-06 19:25:53 +0530

    Add files via upload

---
+++ Question6.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Question-6</h1>
+    </div>
+</template>
+<script>
+export default{
+    name:"RefsC",
+    data(){
+        return{
+
+        }
+
+    },
+    methods:{
+        myfun(){
+            alert("childfile");
+        }
+    }
+}
+</script>
