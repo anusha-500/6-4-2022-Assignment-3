ea4432eb4f18c5520518cbdbd89f0141fe7ce213
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-06 18:22:04 +0530

    Add files via upload

---
+++ que10.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <head>Assignment-3</head>
+        <h1>Question-10</h1>
+        <p>This is errors Handling</p>
+        <button @click="handle()">catch</button>
+    </div>
+</template>
+<script>
+export default{
+    name:"Q10DD",
+    data(){
+        return{
+
+        }
+    },
+methods:{
+handle() {
+try{
+this.count = this.count * 5;
+console.log("printing count",this.count);
+}
+catch{console.log("Error caught")}
+}
+}
+}
+
+</script>
