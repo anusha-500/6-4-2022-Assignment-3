11ca8b7674ac83d276bb79bceb7407b4f3333d6d
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-06 18:19:48 +0530

    Add files via upload

---
+++ que6.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id=demo>
+        <h1>Question-6</h1>
+        <p>using ref to get data from child to parent</p>
+       <p>name: {{name}}</p>
+       <p> mailid:{{mailid}}</p>
+       <p>password:{{password}}</p>
+       <p>phone no:{{number}}</p>
+       <p>Gender:{{gender}}</p>
+        </div>
+</template>
+<script>
+export default{
+    name:'question6AA',
+    props:{
+        name:String,
+        mailid:String,
+        password:String,
+        number:{default:"1234567890"},
+        gender:{default:"female"},
+
+    },
+}
+</script>
