874b5ad7ce5bb783b6dbd813e0e886073e7d77a9
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-07 19:49:33 +0530

    Add files via upload

---
+++ slot12.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>using slots in vue</h1>
+        <p> this is for how to use slots in vuejs</p>
+        <p>this is the child compopnent</p>
+        <slot></slot>
+        </div>
+</template>
+<script>
+export default{
+    name:"SlotsAA",
+    data(){
+        return{
+
+
+        }
+    }
+
+}
+</script>
