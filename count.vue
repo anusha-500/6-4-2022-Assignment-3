1b176b27182d998b0c1cc4372f44ea4e729ed3ef
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-07 19:47:24 +0530

    Add files via upload

---
+++ count.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2> count number of times when button clicked</h2>
+        <button @click="counter++">count</button>
+        <p>Button clicked {{counter}} times!</p>
+    </div>
+
+</template>
+<script>
+export default{
+    name:"CountAa",
+    data(){
+        return{
+            counter:0
+        }
+
+    }
+}
+</script>
