5e9cb42efe7c04c11404ff729ec26459283f1400
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-07 19:49:47 +0530

    Add files via upload

---
+++ timestamp.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <v-container fluid>
+            <h1>{{TimeStamp}}</h1>
+        </v-container>
+        <h2>Displaying current time using timestamp in vue</h2>
+        </div>
+</template>
+<script lang="ts">
+export default{
+    name:"TimeStamp",
+    data(){
+        return{
+        get TimeStamp(){ 
+         const today = new Date()
+            const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+            const TimeStamp =time
+            return TimeStamp;
+        }
+
+        }
+    },
+    methods:{
+       func(){
+        setTimeout(this.TimeStamp,5000)
+       }
+    }
+}
+</script>
+
