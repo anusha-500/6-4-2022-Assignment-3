c4d32b8d0528c2e60782eb14e3edbebebce94a19
anusha-500 <102019002+anusha-500@users.noreply.github.com>
2022-04-07 19:51:55 +0530

    Add files via upload

---
+++ textarea.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>this is for storing multiple lines of data in textarea</h1>
+        <span>multiline text area</span>
+        <p style="white-space: pre-line;">{{ textdata }}</p>
+	<textarea v-model="textdata" placeholder="add multiple lines" required></textarea>
+        </div>
+</template>
+<script>
+export default{
+    name:"DataAA",
+    data(){
+        return{
+            textdata:" "
+        }
+    }
+}
+</script>
